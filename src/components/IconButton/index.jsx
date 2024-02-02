760b4eec731cc918d119f2db3bc3804fac99147d
smithnj13 <SmithNJ13@hotmail.com>
2024-02-02 23:51:58 +0000

    deploy work now?

--- src/components/IconButton/index.jsx
+++ src/components/IconButton/index.jsx
@@ -1,4 +1,3 @@
-import {Circle, Triangle} from "react-shapes"
 import {useNavigate} from "react-router-dom"
 import "./style.css"
 
@@ -12,10 +11,8 @@ const IconButton = () => {
     <>
     <div id="IconContainer">
         <div className="IconBackground">
-            <Triangle width={222} height={178} fill={{color: "#1f456E"}} />
         </div>
         <div className="IconForeground">
-            <Circle r={70} fill={{color: "#e3e4e6"}} />
             <div className="IconImage">
                 <img src="../src/assets/logo.png" onClick={onClick}></img>
             </div>
