59d6ffe0e8f480a06befaacee14a97c695791a82
SmithNJ13 <smithNJ13@hotmail.com>
2025-02-13 23:29:57 +0000

    filled out subsections on each page

--- src/pages/DnD/index.jsx
+++ src/pages/DnD/index.jsx
@@ -28,17 +28,9 @@ const DnD = (hash) => {
             </section>
             <section id="community" className="p-[2rem] flex sm:flex-row-reverse flex-col gap-[1rem] bg-neutral-600 w-full">
               <img src={placeholder} className="sm:w-[30rem] sm:h-[30rem] border-cyan-300 border-2 rounded sm:m-[2rem]"></img>
-              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem] text-end">OUR COMMUNITY
+              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem] text-end">OUR CAMPAIGNS
                 <div className="sm:text-xl text-lg text-white font-normal sm:ml-[2rem]">
                   <p id="smallText" className="bg-cyan-300 bg-opacity-20 p-[1rem] rounded-[1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam.</p>
-                </div>
-              </h2>
-            </section>
-            <section id="extra" className="p-[2rem] flex sm:flex-row flex-col gap-[1rem] bg-neutral-500 w-full">
-              <img src={placeholder} className="sm:w-[30rem] sm:h-[30rem] border-cyan-300 border-2 rounded sm:m-[2rem]"></img>
-              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem]">EXTRA SECTION
-                <div className="sm:text-xl text-lg text-white font-normal sm:mr-[2rem]">
-                  <p id="smallText" className="bg-cyan-300 bg-opacity-20 p-[1rem] rounded-[1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptates maiores inventore velit!</p>
                 </div>
               </h2>
             </section>
--- src/pages/Palworld/index.jsx
+++ src/pages/Palworld/index.jsx
@@ -20,7 +20,7 @@ const Palworld = (hash) => {
             </h1>
             <section id="about" className="p-[2rem] flex sm:flex-row flex-col gap-[1rem] bg-neutral-500 w-full">
               <img src={placeholder} className="sm:w-[30rem] sm:h-[30rem] border-cyan-300 border-2 rounded sm:m-[2rem]"></img>
-              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem]">ABOUT US
+              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem]">MINERVAN VANGUARD IN PALWORLD
                 <div className="sm:text-xl text-lg text-white font-normal sm:mr-[2rem]">
                   <p id="smallText" className="bg-cyan-300 bg-opacity-20 p-[1rem] rounded-[1rem]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, obcaecati!</p>
                 </div>
@@ -28,17 +28,25 @@ const Palworld = (hash) => {
             </section>
             <section id="community" className="p-[2rem] flex sm:flex-row-reverse flex-col gap-[1rem] bg-neutral-600 w-full">
               <img src={placeholder} className="sm:w-[30rem] sm:h-[30rem] border-cyan-300 border-2 rounded sm:m-[2rem]"></img>
-              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem] text-end">OUR COMMUNITY
+              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem] text-end">OUR TEAM
                 <div className="sm:text-xl text-lg text-white font-normal sm:ml-[2rem]">
                   <p id="smallText" className="bg-cyan-300 bg-opacity-20 p-[1rem] rounded-[1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam.</p>
                 </div>
               </h2>
             </section>
-            <section id="extra" className="p-[2rem] flex sm:flex-row flex-col gap-[1rem] bg-neutral-500 w-full">
+            <section id="server" className="p-[2rem] flex sm:flex-row flex-col gap-[1rem] bg-neutral-500 w-full">
               <img src={placeholder} className="sm:w-[30rem] sm:h-[30rem] border-cyan-300 border-2 rounded sm:m-[2rem]"></img>
-              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem]">EXTRA SECTION
+              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem]">SERVER RULES
                 <div className="sm:text-xl text-lg text-white font-normal sm:mr-[2rem]">
                   <p id="smallText" className="bg-cyan-300 bg-opacity-20 p-[1rem] rounded-[1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptates maiores inventore velit!</p>
+                </div>
+              </h2>
+            </section>
+            <section id="serverAbout" className="p-[2rem] flex sm:flex-row-reverse flex-col gap-[1rem] bg-neutral-600 w-full">
+              <img src={placeholder} className="sm:w-[30rem] sm:h-[30rem] border-cyan-300 border-2 rounded sm:m-[2rem]"></img>
+              <h2 className="sm:text-2xl text-xl text-cyan-300 font-bold flex-col sm:my-[4rem] text-end">ABOUT OUR SERVER
+                <div className="sm:text-xl text-lg text-white font-normal sm:ml-[2rem]">
+                  <p id="smallText" className="bg-cyan-300 bg-opacity-20 p-[1rem] rounded-[1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam.</p>
                 </div>
               </h2>
             </section>
